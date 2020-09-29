af07cabb3f23a04e009a7cf35096d6206f3a85c8
Jemyishai <jesse.moskowitz@gmail.com>
2020-09-29 09:54:47 -0400

    Phonebook backend step4

--- index.js
+++ index.js
@@ -1,55 +1,67 @@
-const express = require('express')
-const app = express()
+const { response } = require("express");
+const express = require("express");
+const app = express();
 
 let notes = [
   {
-    "name": "Arto Hellas",
-    "number": "040-123456",
-    "id": 1
+    name: "Arto Hellas",
+    number: "040-123456",
+    id: 1,
   },
   {
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523",
-    "id": 2
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+    id: 2,
   },
   {
-    "name": "Dan Abramov",
-    "number": "12-43-234345",
-    "id": 3
+    name: "Dan Abramov",
+    number: "12-43-234345",
+    id: 3,
   },
   {
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122",
-    "id": 4
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+    id: 4,
+  },
+  {
+    name: "Testy McTesterson",
+    number: "39-23-6423122",
+    id: 5,
+  },
+];
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World!</h1>");
+});
+
+app.get("/api/persons", (req, res) => {
+  res.json(notes);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const note = notes.find((note) => note.id === id);
+
+  if (note) {
+    res.json(note);
+  } else {
+    res.status(404).end();
   }
-]
+});
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
-})
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  notes = notes.filter((note) => note.id !== id);
 
+  res.status(204).end();
+});
 
-app.get('/api/persons', (req, res) => {
-  res.json(notes)
-})
-
-app.get('/api/persons/:id',(req,res)=>{
-  const id = Number(req.params.id)
-  const note = notes.find(note=> note.id === id)
-
-  if (note){
-    res.json(note)
-  } else{
-    res.status(404).end()
-  }
-})
-
-app.get('/info',(req,res)=>{
+app.get("/info", (req, res) => {
   let num = notes.length;
-  res.send(`<p>Phonebook has info for ${num} people</p>`+Date())
-})
+  res.send(`<p>Phonebook has info for ${num} people</p>` + Date());
+});
 
-const PORT = 3001
+const PORT = 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
