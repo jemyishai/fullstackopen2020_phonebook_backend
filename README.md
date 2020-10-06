56638c1e9bc87f049df1b2b20684bc02ab828fd9
Jemyishai <jesse.moskowitz@gmail.com>
2020-10-06 08:44:56 -0400

    updating middleware

---
+++ README.md
@@ -0,0 +1,1 @@
+https://fullstackopen2020phonebook.herokuapp.com/
--- index.js
+++ index.js
@@ -40,6 +40,7 @@ const requestLogger = (request, response, next) => {
   next();
 };
 
+app.use(express.static('build'))
 app.use(cors())
 app.use(express.json());
 app.use(requestLogger);
