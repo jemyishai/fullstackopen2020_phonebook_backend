713a694e8d5760a1c20acfd71e0867790cea0d0c
Jemyishai <jesse.moskowitz@gmail.com>
2020-10-06 08:26:43 -0400

    heroku deploy

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-// const { response } = require("express");
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
+const cors = require('cors')
 
 let persons = [
   {
@@ -30,7 +31,34 @@ let persons = [
   },
 ];
 
-app.use(express.json())
+const requestLogger = (request, response, next) => {
+  console.log('--- Custom Middleware ---')
+  console.log("Method:", request.method);
+  console.log("Path: ", request.path);
+  console.log("Body: ", request.body);
+  console.log("---");
+  next();
+};
+
+app.use(cors())
+app.use(express.json());
+app.use(requestLogger);
+morgan.token('body', (req,res) => JSON.stringify(req.body))
+// app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+// getting incoming body as opposed to body after post route
+// app.use(morgan(":body",{ immediate: true }))
+
+// //req.body here contains id added from post route
+// app.use(morgan( (tokens, req, res) => [
+//     tokens.method(req, res),
+//     tokens.url(req, res),
+//     tokens.status(req, res),
+//     tokens.req(req, res, 'content-length'), '-',
+//     tokens['response-time'](req, res), 'ms',
+//     JSON.stringify(req['body'])
+//   ].join(' ')
+// ));
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello World!</h1>");
@@ -47,7 +75,7 @@ app.get("/api/persons/:id", (req, res) => {
   if (note) {
     res.json(note);
   } else {
-    res.status(404).end();
+    res.status(404).send({ error: "no such ID" }).end();
   }
 });
 
@@ -63,27 +91,49 @@ app.get("/info", (req, res) => {
   res.send(`<p>Phonebook has info for ${num} people</p>` + Date());
 });
 
-
-app.post('/api/persons', (req,res)=> {
-  getRandomInt = (max) =>  Math.floor(Math.random() * Math.floor(max));
-  let ids = persons.map(person=>person.id)
+app.post("/api/persons", (req, res) => {
+  getRandomInt = (max) => Math.floor(Math.random() * Math.floor(max));
+  let ids = persons.map((person) => person.id);
   let tempId = 1;
+  let errorMessage = "";
+  let statusCode = 400;
 
   while (ids.includes(tempId)) {
-    tempId = getRandomInt(10000)
+    tempId = getRandomInt(10000);
   }
 
-  const person = req.body
-  person.id = tempId
+  const personData = req.body;
+  const newPerson = {...personData,id: tempId}
 
-  console.log('person',person)
-  persons = [...persons,person];
 
-  console.log(persons)
-  res.json(persons)
-})
+  const { name, number } = newPerson;
 
-const PORT = 3001;
+  if (!name && !number) {
+    errorMessage = "name and number are missing";
+  } else if (!name) {
+    errorMessage = "name is missing";
+  } else if (!number) {
+    errorMessage = "number is missing";
+  } else if (persons.some((person) => person.name === name)) {
+    errorMessage = "name already exists in the phonebook";
+    statusCode = 409;
+  }
+
+  if (!errorMessage) {
+    persons = [...persons, newPerson];
+    return res.json(persons);
+  } else {
+    return res.status(statusCode).json({ error: errorMessage });
+  }
+});
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
