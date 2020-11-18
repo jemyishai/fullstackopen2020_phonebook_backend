73656ecdb8f4015f74e229d0700b6a73d68772ac
Jemyishai <jesse.moskowitz@gmail.com>
2020-11-17 22:57:48 -0500

    new frontend build

---
+++ build/precache-manifest.d187026566f9c87523c6e47d8463eece.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1903713160cec098330a2e13d0a42b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5c2fdec761cbde4a3d6",
+    "url": "/static/css/main.4b1cc382.chunk.css"
+  },
+  {
+    "revision": "c0561943e19394f17379",
+    "url": "/static/js/2.44d5a009.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.44d5a009.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5c2fdec761cbde4a3d6",
+    "url": "/static/js/main.16494e33.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
