7f1af34a96a74188130ff240637dcfd07ee4396b
Jemyishai <jesse.moskowitz@gmail.com>
2020-11-25 10:58:40 -0500

    exercise 3.12

--- mongo.js
+++ mongo.js
@@ -1,49 +1,48 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 if (process.argv.length < 3) {
-  console.log('Please provide the password as an argument: node mongo.js <password>')
-  process.exit(1)
+  console.log(
+    "Please provide the password as an argument: node mongo.js <password>"
+  );
+  process.exit(1);
 }
 
-const password = process.argv[2]
+const password = process.argv[2];
+const name = process.argv[3];
+const number = process.argv[4];
 
+if (password) {
+  const url = `mongodb+srv://fullstackopen:${password}@cluster0.t0jhq.mongodb.net/allTheNotes?retryWrites=true&w=majority`;
+  mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  });
+}
 
-// const url =
-//   `mongodb+srv://fullstack:${password}@cluster0-ostce.mongodb.net/test?retryWrites=true`
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
 
-// previous URI
-// `mongodb+srv://new_jesse:${password}@cluster0.bwdko.mongodb.net/phonebook?retryWrites=true&w=majority`
+const Person = mongoose.model("Person", personSchema);
 
-// URI from 11/25
-// mongodb+srv://fullstackopen:<password>@cluster0.t0jhq.mongodb.net/<dbname>?retryWrites=true&w=majority
+if (password && name && number) {
+  const person = new Person({
+    name: name,
+    number: number,
+  });
 
-const url = `mongodb+srv://fullstackopen:${password}@cluster0.t0jhq.mongodb.net/allTheNotes?retryWrites=true&w=majority`
-
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-
-const noteSchema = new mongoose.Schema({
-  content: String,
-  date: Date,
-  important: Boolean,
-})
-
-const Note = mongoose.model('Note', noteSchema)
-
-// const note = new Note({
-//   content: 'HTML is Easy',
-//   date: new Date(),
-//   important: true,
-// })
-
-// note.save().then(result => {
-//   console.log('note saved!')
-//   mongoose.connection.close()
-// })
-
-
-Note.find({}).then(result =>{
-  result.forEach(note =>{
-    console.log(note)
-  })
-  mongoose.connection.close()
-})
+  person.save().then((result) => {
+    console.log(`added ${name} number ${number} to phonebook`);
+    mongoose.connection.close();
+  });
+} else if (password && !name && !number) {
+  Person.find({}).then((result) => {
+    result.forEach((person) => {
+      console.log(`${person.name} ${person.number}`);
+    });
+    mongoose.connection.close();
+  });
+}
