69bc3808c8ff70339042c688b7d0beac28b27b95
Jemyishai <jesse.moskowitz@gmail.com>
2020-11-25 18:33:39 -0500

    cleaned up avec prettier

--- index.js
+++ index.js
@@ -6,34 +6,6 @@ const cors = require("cors");
 const Person = require("./models/person");
 const { response } = require("express");
 
-// let persons = [
-//   {
-//     name: "Arto Hellas",
-//     number: "040-123456",
-//     id: 1,
-//   },
-//   {
-//     name: "Ada Lovelace",
-//     number: "39-44-5323523",
-//     id: 2,
-//   },
-//   {
-//     name: "Dan Abramov",
-//     number: "12-43-234345",
-//     id: 3,
-//   },
-//   {
-//     name: "Mary Poppendieck",
-//     number: "39-23-6423122",
-//     id: 4,
-//   },
-//   {
-//     name: "Testy McTesterson",
-//     number: "39-555-6423122",
-//     id: 5,
-//   },
-// ];
-
 const requestLogger = (request, response, next) => {
   console.log("--- Custom Middleware ---");
   console.log("Method:", request.method);
@@ -43,7 +15,7 @@ const requestLogger = (request, response, next) => {
   next();
 };
 
-// app.use(express.static("build"));
+app.use(express.static("build"));
 app.use(cors());
 app.use(express.json());
 app.use(requestLogger);
