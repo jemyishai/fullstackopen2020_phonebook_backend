d4324df6b35ccbde6a5a9ac4137696f8ee7c8b84
Jemyishai <jesse.moskowitz@gmail.com>
2020-11-25 10:33:51 -0500

    logging find operation

--- mongo.js
+++ mongo.js
@@ -11,7 +11,13 @@ const password = process.argv[2]
 // const url =
 //   `mongodb+srv://fullstack:${password}@cluster0-ostce.mongodb.net/test?retryWrites=true`
 
-const url = `mongodb+srv://new_jesse:${password}@cluster0.bwdko.mongodb.net/phonebook?retryWrites=true&w=majority`
+// previous URI
+// `mongodb+srv://new_jesse:${password}@cluster0.bwdko.mongodb.net/phonebook?retryWrites=true&w=majority`
+
+// URI from 11/25
+// mongodb+srv://fullstackopen:<password>@cluster0.t0jhq.mongodb.net/<dbname>?retryWrites=true&w=majority
+
+const url = `mongodb+srv://fullstackopen:${password}@cluster0.t0jhq.mongodb.net/allTheNotes?retryWrites=true&w=majority`
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
@@ -23,13 +29,21 @@ const noteSchema = new mongoose.Schema({
 
 const Note = mongoose.model('Note', noteSchema)
 
-const note = new Note({
-  content: 'HTML is Easy',
-  date: new Date(),
-  important: true,
-})
+// const note = new Note({
+//   content: 'HTML is Easy',
+//   date: new Date(),
+//   important: true,
+// })
 
-note.save().then(result => {
-  console.log('note saved!')
+// note.save().then(result => {
+//   console.log('note saved!')
+//   mongoose.connection.close()
+// })
+
+
+Note.find({}).then(result =>{
+  result.forEach(note =>{
+    console.log(note)
+  })
   mongoose.connection.close()
 })
